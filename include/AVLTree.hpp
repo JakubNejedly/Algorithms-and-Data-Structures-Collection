d5439cde7ca994a34fd3fc4bd45a348e8bf42fb1
Jakub Nejedly <jakub.nejedly@gmail.com>
2026-01-11 11:34:55 +0100

    Add AVLTree

--- include/AVLTree.hpp
+++ include/AVLTree.hpp
@@ -6,15 +6,30 @@ template <typename T, typename Comparator = std::less<T>>
 class AVLTree : public BinaryTree<T, Comparator> {
 public:
     AVLTree(Comparator comparator = Comparator());
+    ~AVLTree() = default;
 
     void insert(T data) override;
-    // void remove(const T& data) override;
+    void remove(const T& data) override;
+    T removeMin() override;
+    T removeMax() override;
 
 private:
+    using SortingTree<T, Comparator>::m_root;
+    
     using TreeNodePtr = typename BinaryTree<T, Comparator>::TreeNodePtr;
 
-    void recursive_insert_avl(TreeNodePtr& node, T data);
-    // TreeNodePtr recursive_remove_avl(TreeNodePtr node, const T& data);
+    // Helper for insert
+    void recursive_insert(TreeNodePtr& node, T data);
+
+    // Helpers for remove
+    void recursive_remove(TreeNodePtr& node, const T& data);
+    TreeNodePtr extractMin(TreeNodePtr& node); 
+
+    // Helper for removeMin
+    T recursive_remove_min(TreeNodePtr& node);
+
+    // Helper for removeMax
+    T recursive_remove_max(TreeNodePtr& node);
 
     void rebalance(TreeNodePtr& node);
     
@@ -31,11 +46,11 @@ AVLTree<T, Comparator>::AVLTree(Comparator comparator)
 template <typename T, typename Comparator>
 void AVLTree<T, Comparator>::insert(T data) {
     this->m_elementsCount++;
-    recursive_insert_avl(this->m_root, std::move(data));
+    recursive_insert(m_root, std::move(data));
 }
 
 template <typename T, typename Comparator>
-void AVLTree<T, Comparator>::recursive_insert_avl(TreeNodePtr& node, T data) {
+void AVLTree<T, Comparator>::recursive_insert(TreeNodePtr& node, T data) {
     if (!node) {
         this->m_nodesCount++;
         node = std::make_unique<TreeNode<T>>(std::move(data));
@@ -46,15 +61,141 @@ void AVLTree<T, Comparator>::recursive_insert_avl(TreeNodePtr& node, T data) {
         node->incrementCount();
     } else {
         if (this->m_comparator(data, node->getData())) {
-            recursive_insert_avl(node->getLeft(), std::move(data));
+            recursive_insert(node->getLeft(), std::move(data));
         } else {
-            recursive_insert_avl(node->getRight(), std::move(data));
+            recursive_insert(node->getRight(), std::move(data));
         }
     }
 
     node->updateHeight();
     rebalance(node);
     return;
+}
+
+template <typename T, typename Comparator>
+void AVLTree<T, Comparator>::remove(const T& data) {
+    recursive_remove(m_root, data);
+}
+
+template <typename T, typename Comparator>
+void AVLTree<T, Comparator>::recursive_remove(TreeNodePtr& node, const T& data) {
+    if (!node){
+        // Record not found
+        return;
+    }
+
+    if (this->m_comparator(data, node->getData())) {
+        recursive_remove(node->getLeft(), data);
+    } else if (this->m_comparator(node->getData(), data)) {
+        recursive_remove(node->getRight(), data);
+    } else {
+        // Data found
+        if (node->decrementCount()) {
+            this->m_elementsCount--;
+            return;
+        }
+
+        this->m_elementsCount--;
+        this->m_nodesCount--;
+
+        if (!node->getLeft()) {
+            // Only right child case
+            node = std::move(node->getRight());
+            return;
+        } 
+        else if (!node->getRight()) {
+            // Only left child case
+            node = std::move(node->getLeft());
+            return;
+        } 
+        else {
+            // Two children case
+            TreeNodePtr successor = extractMin(node->getRight());
+            successor->getLeft() = std::move(node->getLeft());
+            successor->getRight() = std::move(node->getRight());
+            node = std::move(successor);
+        }
+    }
+
+    if (node) {
+        node->updateHeight();
+        rebalance(node);
+    }
+}
+
+template <typename T, typename Comparator>
+typename AVLTree<T, Comparator>::TreeNodePtr 
+AVLTree<T, Comparator>::extractMin(TreeNodePtr& node) {
+    if (node->getLeft()) {
+        TreeNodePtr minNode = extractMin(node->getLeft());
+        node->updateHeight();
+        rebalance(node);
+        return minNode;
+    }
+
+    TreeNodePtr minNode = std::move(node);
+    node = std::move(minNode->getRight());
+    return minNode;
+}
+
+template <typename T, typename Comparator>
+T AVLTree<T, Comparator>::removeMin() {
+    if (this->empty()) {
+        throw std::runtime_error("Tree is empty");
+    }
+    this->m_elementsCount--;
+    return std::move(recursive_remove_min(m_root));
+}
+
+template <typename T, typename Comparator>
+T AVLTree<T, Comparator>::recursive_remove_min(TreeNodePtr& node) {
+    // Iterate to find min value
+    if (node->getLeft()) {
+        T outData = std::move(recursive_remove_min(node->getLeft()));
+        node->updateHeight();
+        rebalance(node);
+        return std::move(outData);
+    }
+
+    T outData = std::move(node->getData());
+    if (node->decrementCount()) {
+        // Only decrement count
+        return std::move(outData);
+    }
+    // Replace node with its right child
+    this->m_nodesCount--;
+    node = std::move(node->getRight());
+    return std::move(outData);
+}
+
+template <typename T, typename Comparator>
+T AVLTree<T, Comparator>::removeMax() {
+    if (this->empty()) {
+        throw std::runtime_error("Tree is empty");
+    }
+    this->m_elementsCount--;
+    return std::move(recursive_remove_max(m_root));
+}
+
+template <typename T, typename Comparator>
+T AVLTree<T, Comparator>::recursive_remove_max(TreeNodePtr& node) {
+    // Iterate to find max value
+    if (node->getRight()) {
+        T outData = std::move(recursive_remove_max(node->getRight()));
+        node->updateHeight();
+        rebalance(node);
+        return std::move(outData);
+    }
+
+    T outData = std::move(node->getData());
+    if (node->decrementCount()) {
+        // Only decrement count
+        return std::move(outData);
+    }
+    // Replace node with its left child
+    this->m_nodesCount--;
+    node = std::move(node->getLeft());  
+    return std::move(outData);
 }
 
 template <typename T, typename Comparator>
@@ -93,7 +234,7 @@ void AVLTree<T, Comparator>::rotateLeft(TreeNodePtr& node) {
     node->updateHeight();
     pivot->getLeft() = std::move(node);
     pivot->updateHeight();
-    
+
     node = std::move(pivot);
 }
 
--- tests/test_avl_tree.cpp
+++ tests/test_avl_tree.cpp
@@ -7,7 +7,7 @@ protected:
 };
 
 
-TEST_F(AVLTreeTest, RightRotationLL) {
+TEST_F(AVLTreeTest, RightRotationL) {
     avl.insert(30);
     avl.insert(20);
     avl.insert(10);
@@ -20,7 +20,7 @@ TEST_F(AVLTreeTest, RightRotationLL) {
 }
 
 
-TEST_F(AVLTreeTest, LeftRotationRR) {
+TEST_F(AVLTreeTest, LeftRotationR) {
     avl.insert(10);
     avl.insert(20);
     avl.insert(30);
@@ -54,11 +54,79 @@ TEST_F(AVLTreeTest, DoubleRotationRL) {
     EXPECT_EQ(avl.getRoot()->getRight()->getData(), 30);
 }
 
-TEST_F(AVLTreeTest, BalancedHeightLargeScale) {
-    for (int i = 1; i <= 100; ++i) {
-        avl.insert(i);
+TEST_F(AVLTreeTest, HeightAfterMultipleInsertions) {
+    for (int x : {50, 30, 70, 20, 40, 60, 80}) {
+        avl.insert(x);
     }
 
-    EXPECT_LE(avl.getRoot()->getHeight(), 8);
-    EXPECT_EQ(avl.elementsCount(), 100);
+    ASSERT_NE(avl.getRoot(), nullptr);
+    EXPECT_EQ(avl.getRoot()->getHeight(), 3);
+}
+
+TEST_F(AVLTreeTest, RemoveLeafTriggersRotation) {
+    avl.insert(20);
+    avl.insert(10);
+    avl.insert(30);
+    avl.insert(25);
+    
+    avl.remove(10);
+
+    ASSERT_NE(avl.getRoot(), nullptr);
+    EXPECT_EQ(avl.getRoot()->getData(), 25);
+    EXPECT_EQ(avl.getRoot()->getHeight(), 2);
+    EXPECT_EQ(avl.nodesCount(), 3);
+}
+
+TEST_F(AVLTreeTest, RemoveMinWithRebalance) {
+    for (int x : {10, 20, 30, 40, 50}) avl.insert(x);
+    EXPECT_EQ(avl.getRoot()->getHeight(), 3);
+
+    EXPECT_EQ(avl.removeMin(), 10);
+    EXPECT_EQ(avl.removeMin(), 20);
+
+    EXPECT_LE(avl.getRoot()->getHeight(), 2);
+    EXPECT_EQ(avl.nodesCount(), 3);
+    EXPECT_EQ(avl.getRoot()->getData(), 40);
+    EXPECT_EQ(avl.getRoot()->getLeft()->getData(), 30);
+    EXPECT_EQ(avl.getRoot()->getRight()->getData(), 50);
+}
+
+TEST_F(AVLTreeTest, RemoveMaxWithRebalance) {
+    for (int x : {50, 40, 30, 20, 10}) avl.insert(x);
+    
+    EXPECT_EQ(avl.removeMax(), 50);
+    EXPECT_EQ(avl.removeMax(), 40);
+
+    EXPECT_LE(avl.getRoot()->getHeight(), 2);
+    EXPECT_EQ(avl.nodesCount(), 3);
+    EXPECT_EQ(avl.getRoot()->getData(), 20);
+    EXPECT_EQ(avl.getRoot()->getLeft()->getData(), 10);
+    EXPECT_EQ(avl.getRoot()->getRight()->getData(), 30);
+}
+
+TEST_F(AVLTreeTest, RemoveNodeWithTwoChildrenAVL) {
+    for (int x : {50, 25, 75, 15, 35, 65, 85}) avl.insert(x);
+    
+    avl.remove(50);
+
+    ASSERT_NE(avl.getRoot(), nullptr);
+    EXPECT_EQ(avl.elementsCount(), 6);
+    EXPECT_LE(avl.getRoot()->getHeight(), 3);
+    EXPECT_EQ(avl.getRoot()->getData(), 65);
+}
+
+TEST_F(AVLTreeTest, CascadingRotations) {
+    for (int x : {50, 25, 80, 10, 30, 60, 90, 5, 15, 27, 35, 55}) avl.insert(x);
+    
+    avl.remove(90);
+    
+    ASSERT_NE(avl.getRoot(), nullptr);
+    EXPECT_LE(avl.getRoot()->getHeight(), 4);
+    EXPECT_EQ(avl.getRoot()->getData(), 50);
+    EXPECT_EQ(avl.getRoot()->getLeft()->getData(), 25);
+    EXPECT_EQ(avl.getRoot()->getLeft()->getLeft()->getData(), 10);
+    EXPECT_EQ(avl.getRoot()->getLeft()->getRight()->getData(), 30);
+    EXPECT_EQ(avl.getRoot()->getRight()->getData(), 60);
+    EXPECT_EQ(avl.getRoot()->getRight()->getLeft()->getData(), 55);
+    EXPECT_EQ(avl.getRoot()->getRight()->getRight()->getData(), 80);
 }
